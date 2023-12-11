18f47dc65fe79495131a621f52488b9d7d4d4e58
alexfishy12 <58009009+alexfishy12@users.noreply.github.com>
2023-12-11 11:43:15 -0500

    Reformatted webpage, added AI functionality

--- webpage/_js/script.js
+++ webpage/_js/script.js
@@ -1,66 +1,137 @@
-const waveformContainer = document.getElementById('waveform')
-const audioInput = document.getElementById('audio-input')
-const playButton = document.getElementById('play-button')
-const pauseButton = document.getElementById('pause-button')
-const browseButton = document.getElementById('browse-button');
-const downloadButton = document.getElementById('download-button');
+var waveFormContainer;
+var audioInput;
+var playButton;
+var pauseButton;
+var resetButton;
+var browseButton;
+var downloadButton;
+var waveSurfer;
+var dropArea;
+var midiPlayer;
+var midiVisualizer;
 
-const wavesurfer = WaveSurfer.create({
-    container: waveformContainer,
-    waveColor: '#00ccff',
-    progressColor: '#0088cc'
+// when document is ready
+$.when( $.ready ).then(function() {
+    initialize_waveform();
+    initialize_audio_input();
+    initialize_buttons();
+    initialize_midi();
 });
 
-const dropArea = document.getElementById('drop-area');
+function initialize_buttons() {
+    playButton = document.getElementById('play-button')
+    pauseButton = document.getElementById('pause-button')
+    browseButton = document.getElementById('browse-button');
+    resetButton = document.getElementById('reset-button');
+    downloadButton = document.getElementById('download-button');
 
-dropArea.addEventListener('dragover', function (event) {
-    event.preventDefault();
-    dropArea.style.backgroundColor = 'rgb(132, 129, 129)';
-});
+    playButton.addEventListener('click', function () {
+        waveSurfer.play()
+    });
+    
+    pauseButton.addEventListener('click', function () {
+        waveSurfer.pause()
+    });
 
-dropArea.addEventListener('dragleave', function () {
-    dropArea.style.backgroundColor = 'rgb(132, 129, 129)';
-});
+    resetButton.addEventListener('click', function () {
+        // put needle back to start of audio clip
+        waveSurfer.stop()
+    });
 
-dropArea.addEventListener('drop', function (event) {
-    event.preventDefault();
-    dropArea.style.backgroundColor = 'rgb(132, 129, 129)';
-
-    const files = event.dataTransfer.files;
-    handleFiles(files);
-});
-
-audioInput.addEventListener('change', function () {
-    const files = this.files;
-    handleFiles(files);
-});
-
-function handleFiles(files) {
-    if (!files || !files[0]) {
-        return;
-    }
-    const fileURL = URL.createObjectURL(files[0])
-    wavesurfer.load(fileURL)
+    downloadButton.addEventListener('click', function () {
+        // download midi file from URL
+        const url = midiPlayer.prop("src");
+        const filename = url.split('/').pop();
+        download(filename, url); 
+    });
 }
 
-playButton.addEventListener('click', function () {
-    wavesurfer.play()
-});
+function download(filename, url) {
+    // create invisible link element
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    document.body.appendChild(link);
 
-pauseButton.addEventListener('click', function () {
-    wavesurfer.pause()
-});
+    // set link href to url
+    link.href = url;
+    link.download = filename;
 
-browseButton.addEventListener('click', function () {
-    audioInput.click();
-});
-downloadButton.addEventListener('click', function () {
-    // Assuming wavesurfer.load('./assets/ThisOldGuitar.mid') is used initially
-    const midiFileURL = '../_assets/ThisOldGuitar.mid';
-    const downloadLink = document.createElement('a');
-    downloadLink.href = midiFileURL;
-    downloadLink.download = 'downloaded.mid';
-    document.body.appendChild(downloadLink);
-    downloadLink.click();
-    document.body.removeChild(downloadLink);
-});
+    // click link to download file
+    link.click();
+
+    // remove link from DOM
+    document.body.removeChild(link);
+
+}
+
+function initialize_audio_input() {
+    audioInput = document.getElementById('audio-input');
+    dropArea = document.getElementById('drop-area');
+
+    dropArea.addEventListener('dragover', function (event) {
+        event.preventDefault();
+        dropArea.style.backgroundColor = 'rgb(132, 129, 129)';
+    });
+    
+    dropArea.addEventListener('dragleave', function () {
+        dropArea.style.backgroundColor = 'rgb(132, 129, 129)';
+    });
+    
+    dropArea.addEventListener('drop', function (event) {
+        event.preventDefault();
+        dropArea.style.backgroundColor = 'rgb(132, 129, 129)';
+    
+        const files = event.dataTransfer.files;
+        handleFiles(files);
+    });
+    
+    audioInput.addEventListener('change', function () {
+        const files = this.files;
+        handleFiles(files);
+    });
+    
+    function handleFiles(files) {
+        console.log("Handling files...")
+        const label = $("#file_label");
+        if (!files || !files[0]) {
+            label.html("Drag & drop your audio file here <br> or");
+            reset_waveform();
+            return;
+        }
+        const fileURL = URL.createObjectURL(files[0])
+        waveSurfer.load(fileURL)
+
+        // set label to filename
+        const filename = files[0].name;
+        label.html("Selected file: " + filename + "<br><br>");
+    }
+}
+
+function initialize_waveform() {
+    waveformContainer = document.getElementById('waveform-container')
+    
+    waveSurfer = WaveSurfer.create({
+        container: waveformContainer,
+        waveColor: '#00ccff',
+        progressColor: '#0088cc'
+    });
+}
+
+function reset_waveform() {
+    waveSurfer.empty();
+}
+
+function initialize_midi() {
+    midiPlayer = $("#midi-player");
+    midiVisualizer = $("#midi-visualizer");   
+}
+
+function load_midi(filepath) {
+    midiPlayer.prop("src", filepath);
+    midiVisualizer.prop("src", filepath);
+}
+
+function reset_midi() {
+    midiPlayer.prop("src", "");
+    midiVisualizer.prop("src", "");
+}

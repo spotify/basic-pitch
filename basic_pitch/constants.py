fdb4a807186c0aed721c402e9843efb97784fbaa
Benjie Genchel <benjiegenchel@gmail.com>
2024-08-16 11:29:59 -0400

    small format change in constants.py re: Split enum

--- basic_pitch/constants.py
+++ basic_pitch/constants.py
@@ -62,6 +62,7 @@ def _freq_bins(bins_per_semitone: int, base_frequency: float, n_semitones: int) 
 FREQ_BINS_NOTES = _freq_bins(NOTES_BINS_PER_SEMITONE, ANNOTATIONS_BASE_FREQUENCY, ANNOTATIONS_N_SEMITONES)
 FREQ_BINS_CONTOURS = _freq_bins(CONTOURS_BINS_PER_SEMITONE, ANNOTATIONS_BASE_FREQUENCY, ANNOTATIONS_N_SEMITONES)
 
+
 class Split(Enum):
     train = "train"
     validation = "validation"

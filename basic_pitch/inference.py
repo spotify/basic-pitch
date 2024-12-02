f5b5dc9f61a536b7d601b9ba22dfb7ef6d58b921
marypilataki <marypilataki@gmail.com>
2024-12-03 12:04:36 +1300

    add comments

--- basic_pitch/inference.py
+++ basic_pitch/inference.py
@@ -228,16 +228,18 @@ def get_audio_input(
 
     """
     assert overlap_len % 2 == 0, f"overlap_length must be even, got {overlap_len}"
-
+    # if a numpy array of samples is provided, use it directly
     if isinstance(audio_path_or_array, np.ndarray):
         audio_original = audio_path_or_array
         if sample_rate is None:
             raise ValueError("Sample rate must be provided when input is an array of audio samples.")
+        # resample audio if required
         elif sample_rate != AUDIO_SAMPLE_RATE:
             audio_original = librosa.resample(audio_original, orig_sr=sample_rate, target_sr=AUDIO_SAMPLE_RATE)
         # convert to mono if necessary
         if audio_original.ndim != 1:
             audio_original = librosa.to_mono(audio_path_or_array)
+    # load audio file
     else:
         audio_original, _ = librosa.load(str(audio_path_or_array), sr=AUDIO_SAMPLE_RATE, mono=True)
 
